0b4eec25c458e254ab3a5e8f4f32b4634f999ee6
joyegwuatu <egwuatujoy60@gmail.com>
2024-07-26 21:26:56 +0100

    question two, day three

--- daythree.js
+++ daythree.js
@@ -4,3 +4,16 @@ function strUppercase(str) {
   return str.toUpperCase();
 }
 console.log(strUppercase("Hello"));
+
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+function iniTial(fullname) {
+  return fullname.slice(0, 1).toUpperCase() + "." + fullname.split(" ")[1].slice(0, 1).toUpperCase();
+}
+
+console.log(iniTial("joy egwuatu"));
+console.log(iniTial("Chioma Peter"));
+console.log(iniTial("Victor Moses"));
+
