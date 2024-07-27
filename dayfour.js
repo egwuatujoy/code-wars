32bd3343db624c7be70b1288552a2855f788f450
joyegwuatu <egwuatujoy60@gmail.com>
2024-07-27 15:00:42 +0100

    day-two question-two

--- dayfour.js
+++ dayfour.js
@@ -7,5 +7,23 @@ function strJoin(str) {
 
 console.log(strJoin("hello joy hi a b"));
 
+// question two
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
+// es6
+const simpleMultiplication = (num) => (num % 2 === 0 ? num * 8 : num * 9);
 
+console.log(simpleMultiplication(11));
+let txt = "";
+
+// normal js
+function mulNum(num) {
+  if (num % 2 === 0) {
+    num = num * 8;
+  } else {
+    num = num * 9;
+  }
+  return num;
+}
+
+console.log(mulNum(10));
